78db6580fed933649337361fa1d63a7c379b240f
cory holenstein <cholensteinwork@gmail.com>
2023-08-25 11:35:06 -0400

    better spot saving and username fix on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,22 +5,34 @@ import { usernameState } from '../atoms';
 import * as React from 'react';
 import { useState } from "react";
 import { TextField, Button } from '@aws-amplify/ui-react';
+import { useGeolocated } from "react-geolocated";
 import axios from 'axios';
 function Home() {
-
-    const [username, setUsername] = useRecoilValue(usernameState);
+    const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+        useGeolocated({
+            positionOptions: {
+                enableHighAccuracy: false,
+            },
+            userDecisionTimeout: 5000,
+        });
+    const username = useRecoilValue(usernameState);
     const [spotName, setSpotName] = useState("");
     const [streetName, setStreetName] = useState("");
     const [spotNotes, setSpotNotes] = useState("");
+    const [spotNumber, setSpotNumber] = useState("");
+    const [spotLevel, setSpotLevel] = useState("");
 
 
     const saveSpot = (event) => {
         event.preventDefault();
         axios.post('/savespot', {
-            username: "username",
+            username: username,
             spotName: spotName,
             streetName: streetName,
-            spotNotes: spotNotes
+            spotNotes: spotNotes,
+            latitude: coords.latitude,
+            longitude: coords.longitude,
+            altitude: coords.altitude
         })
             .then(function (response) {
                 console.log(response);
@@ -30,7 +42,9 @@ function Home() {
             });
     } 
 
-
+    if (!isGeolocationAvailable || !isGeolocationEnabled) {
+        return <div>Geolocation not available / enabled!</div>
+    } else {
 
     return (
         <div>
@@ -44,7 +58,19 @@ function Home() {
                 errorMessage="There is an error"
                 onChange={e => setSpotName(e.target.value)}
 
-            />
+             />
+             <TextField
+                    placeholder="Spot Number"
+                    label="Spot Number"
+                    errorMessage="There is an error"
+                    onChange={e => setSpotNumber(e.target.value)}
+                />
+            <TextField
+                    placeholder="Spot Level"
+                    label="Spot Level"
+                    errorMessage="There is an error"
+                    onChange={e => setStreetName(e.target.value)}
+             />
             <TextField
                 placeholder="Street Name"
                 label="Street name"
@@ -61,7 +87,8 @@ function Home() {
             </form>
         </div>
 
-    );
+        );
+    }
 }
 
 export default Home;
